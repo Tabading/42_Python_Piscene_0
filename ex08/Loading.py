ed3de7ef0278057dd45866febaacbc16d95d70d3
Tabading <tarlaxo@gmail.com>
2026-08-17 14:13:30 +0200

    ex08 normed

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -1,31 +1,37 @@
-
 import os
 
-# width			: (total terminal width) - 5(100%|) -  (| (333)/(333) ) - 26([00:01<00:00, 177.24it/s](\n))
-
+# width:
+#   (total terminal width) - 5(100%|) -
+#   (| (333)/(333) ) -
+#   26([00:01<00:00, 177.24it/s](\n))
 # fsting:
-# \r			: carriage return, move cursor back to beginning line
-# :3			: minimum width, ex: '  5' ' 50' '100'
-# :<20			: left aligned width 20
-# end=""		: no '\n' 
-# flush=True	: output imidiatly
+# \r            : carriage return, move cursor back to beginning line
+# :3            : minimum width, ex: '  5' ' 50' '100'
+# :<20          : left aligned width 20
+# end=""        : no '\n'
+# flush=True    : output imidiatly
+
 
 # like original with ███
 def ft_tqdm(lst: range) -> None:
-	total = len(lst)
-	width = os.get_terminal_size().columns - 5 - (4 + (2 * len(str(total)))) - 26
-	for i, elem in enumerate(lst, 1):
-		p = i * 100 // total
-		bar = "█" * (p * width // 100)
-		print(f"\r{p:3}%|{bar:<{width}}| {i}/{total}", end="", flush=True)
-		yield elem
+    total = len(lst)
+    width = os.get_terminal_size().columns - 5 - \
+        (4 + (2 * len(str(total)))) - 26
+    for i, elem in enumerate(lst, 1):
+        p = i * 100 // total
+        bar = "█" * (p * width // 100)
+        print(f"\r{p:3}%|{bar:<{width}}| {i}/{total}", end="", flush=True)
+        yield elem
+
 
 # like example with [==>] width - 3 ([>])
 # def ft_tqdm(lst: range) -> None:
-# 	total = len(lst)
-# 	width = os.get_terminal_size().columns - 5 - (4 + (2 * len(str(total)))) - 26 - 3
-# 	for i, elem in enumerate(lst, 1):
-# 		p = i * 100 // total
-# 		bar = "=" * (p * width // 100)
-# 		print(f"\r{p:3}%|[{bar + '>':<{width}}]| {i}/{total}", end="", flush=True)
-# 		yield elem
+#   total = len(lst)
+#   width = os.get_terminal_size().columns - 5 - \
+#       (4 + (2 * len(str(total)))) - 26 - 3
+#   for i, elem in enumerate(lst, 1):
+#       p = i * 100 // total
+#       bar = "=" * (p * width // 100)
+#       print(f"\r{p:3}%|[{bar + '>':<{width}}]| {i}/{total}", \
+#           end="", flush=True)
+#       yield elem
